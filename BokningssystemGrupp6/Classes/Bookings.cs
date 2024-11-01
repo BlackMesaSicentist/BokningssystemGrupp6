f809c4bafe5906a08c5a4b2ea3820ef018e175a0
Echo3301 <angelicabergstroms@gmail.com>
2024-11-01 13:15:44 +0100

    booknings prop
    prop och ctor

--- BokningssystemGrupp6/Classes/Bookings.cs
+++ BokningssystemGrupp6/Classes/Bookings.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace BokningssystemGrupp6.Classes
 {
-    internal class Bookings
+    public class Bookings
     {
+        public string UserName { get; set; }
+        public string RoomName { get; set; }
+        public double StartTime { get; set; }
+        public double EndTime { get; set; }
+
+        public Bookings()
+        {
+            
+        }
+
+        public Bookings(string userName, string roomName, double startTime, double endTime)
+        {
+            UserName = userName;
+            RoomName = roomName;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
     }
 }
