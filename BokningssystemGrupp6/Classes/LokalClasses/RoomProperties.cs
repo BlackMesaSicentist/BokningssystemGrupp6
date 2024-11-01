782682eabfe5781413f235f4c7c97382e6fa084e
AdamLunaAH <adam.a-h@outlook.com>
2024-11-01 12:11:58 +0100

    Room properties with constructor and interface

---
+++ BokningssystemGrupp6/Classes/LokalClasses/RoomProperties.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BokningssystemGrupp6.Interfaces;
+
+namespace BokningssystemGrupp6.Classes.LokalClasses
+{
+    public class RoomProperties : IRoomInterface
+    {
+        public string RoomName { get; set; }
+        public string RoomType { get; set; }
+        public int SeatAmount { get; set; }
+
+        public RoomProperties(string roomName, string roomType, int seatAmount)
+        {
+            RoomName = roomName;
+            RoomType = roomType;
+            SeatAmount = seatAmount;
+        }
+
+
+    }
+
+
+    public class LargeRoomProperties : RoomProperties
+    {
+        public int SeatLimit { get; set; }
+        public bool HasProjector { get; set; }
+        public bool HasWhiteboard { get; set; }
+
+        public LargeRoomProperties(string roomName, string roomType, int seatAmount, int seatLimit, bool hasProjector, bool hasWhiteboard) : base(roomName, roomType, seatAmount)
+        {
+            SeatLimit = seatLimit;
+            HasProjector = hasProjector;
+            HasWhiteboard = hasWhiteboard;
+        }
+
+    }
+
+    public class MediumRoomProperties : RoomProperties
+    {
+        public int SeatLimit { get; set; }
+        public bool HasProjector { get; set; }
+        public bool HasWhiteboard { get; set; }
+
+        public MediumRoomProperties(string roomName, string roomType, int seatAmount, int seatLimit, bool hasProjector, bool hasWhiteboard) : base(roomName, roomType, seatAmount)
+        {
+            SeatLimit = seatLimit;
+            HasProjector = hasProjector;
+            HasWhiteboard = hasWhiteboard;
+        }
+    }
+    public class SmallRoomProperties : RoomProperties
+    {
+        public int SeatLimit { get; set; }
+
+        public SmallRoomProperties(string roomName, string roomType, int seatAmount, int seatLimit) : base(roomName, roomType, seatAmount)
+        {
+            SeatLimit = seatLimit;
+        }
+    }
+}
---
+++ BokningssystemGrupp6/Interfaces/IRoomInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BokningssystemGrupp6.Interfaces
+{
+    public interface IRoomInterface
+    {
+        string RoomName { get; set; }
+        string RoomType { get; set; }
+        int SeatAmount { get; set; }
+    }
+}
