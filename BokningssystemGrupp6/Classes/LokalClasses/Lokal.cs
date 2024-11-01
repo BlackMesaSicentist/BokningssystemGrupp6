ee4284a2ffa39dda64187be988daba039b968a24
DavidBerglin <david.berglin@gmail.com>
2024-11-01 13:27:26 +0100

    Createroom2

--- BokningssystemGrupp6/Classes/LokalClasses/Lokal.cs
+++ BokningssystemGrupp6/Classes/LokalClasses/Lokal.cs
@@ -8,15 +8,17 @@ namespace BokningssystemGrupp6.Classes.LokalClasses
 {
     internal class Rooms
     {
-        public static void CreateRoom(List<Rooms> rooms)
+        public static void CreateARoom(List<Rooms> rooms)
         {
             Console.WriteLine("Enter name of the room: ");
             string? roomName = Console.ReadLine();
             string roomSize = RoomSize();
-            Console.WriteLine("Enter size of room: ");
+            Console.WriteLine("Enter how many seats:  ");
             int size = Convert.ToInt32(Console.ReadLine());
-            bool hasProjector = AskUser("Do you need a projector? Y/N");
-            bool hasWhiteBoard = AskUser("Do you need a whiteboard? Y/N");
+            Console.WriteLine("Do you need a projector? Y/N");
+            bool hasProjector = AskUser();
+            Console.WriteLine("Do you need a whiteboard? Y/N");
+            bool hasWhiteBoard = AskUser();
 
             Rooms room = new Rooms(roomName, roomSize, size, hasProjector, hasWhiteBoard);
             rooms.Add(room);
@@ -24,12 +26,12 @@ namespace BokningssystemGrupp6.Classes.LokalClasses
         public static string RoomSize()
         {
        
-            Console.WriteLine("1.Small\n2.Medium\n3.Large");
+            Console.WriteLine("1.Grupprum\n2.Medium\n3.Large");
             string? size;
             switch (Console.ReadLine())
             {
                 case "1":
-                    size = "Small";
+                    size = "Grupprum";
                     break;
                 case "2":
                     size = "Medium";
@@ -39,12 +41,11 @@ namespace BokningssystemGrupp6.Classes.LokalClasses
                     break;
                 default:
                     Console.WriteLine("Invalid choice");
-                    return RoomSize();
-                    
+                    return RoomSize();                    
             }
             return size;
         }
-        public static bool AskUser(string ask)
+        public static bool AskUser()
         {
             while (true)
             {
