c98e74980d940d117b37713947519e65c38ea3fb
BlackMesaSicentist <alex.bullerjahn.code@outlook.com>
2024-11-01 12:19:05 +0100

    Added all the menu choices, added som comments and a todo

--- BokningssystemGrupp6/Classes/Menu.cs
+++ BokningssystemGrupp6/Classes/Menu.cs
@@ -10,7 +10,7 @@ namespace BokningssystemGrupp6.Classes
     {
         public static Menu MainMenu(/*Might need list of Rooms and Bookings depending on what the methods need */)
         {
-            String? menuChoice; // Declared a variable outside the switch, makes the code friendly to modification
+            String? menuChoice; // Declared a variable outside the switch, makes the code friendly to modification, can be removed and placed in the switch statement
             while (true) // Todo: make an exit condition to break loop, either as a universal method or specific in this menu
             {
                 Console.WriteLine("Välkommen till bokningssystemet för skolans lokaler! \nDu har följande alternativ:" +
@@ -18,11 +18,19 @@ namespace BokningssystemGrupp6.Classes
                     "\n4. Se bokningar \n5. Uppdatera en befintlig bokning");
                 switch (menuChoice = Console.ReadLine())
                 {
-                    case "1": Rooms.listAndSortMenu(); break;
+                    case "1": Rooms.listAndSortRooms(); break; 
                     case "2": Rooms.CreateARoom(); break;
-                    case "3": Rooms.
+                    case "3": Bookings.BookARoom(); break;
+                    case "4": Bookings.ListBookings(); break;
+                    case "5": Bookings.UppdateBookings(); break;
+                    default:
+                    {
+                        Console.WriteLine("Inte ett giltigt val, tryck på \"Enter\" och försök igen");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    }
                 }
-                
             }
         }
     }
