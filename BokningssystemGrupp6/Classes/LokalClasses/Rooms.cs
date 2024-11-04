fd2fdd3ecbb57e8545667a0a396324f950633ca4
AdamLunaAH <adam.a-h@outlook.com>
2024-11-04 09:49:11 +0100

    Simple usable Add room function

--- BokningssystemGrupp6/Classes/LokalClasses/Rooms.cs
+++ BokningssystemGrupp6/Classes/LokalClasses/Rooms.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BokningssystemGrupp6.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,8 @@ namespace BokningssystemGrupp6.Classes.LokalClasses
 {
     internal class Rooms
     {
-        public static void CreateARoom(List<Rooms> rooms)
-        {
+
+        public static void CreateARoom2(List<IRoom> rooms) {
             Console.WriteLine("Enter name of the room: ");
             string? roomName = Console.ReadLine();
             string roomSize = RoomSize();
@@ -19,32 +20,96 @@ namespace BokningssystemGrupp6.Classes.LokalClasses
             bool hasProjector = AskUser();
             Console.WriteLine("Do you need a whiteboard? Y/N");
             bool hasWhiteBoard = AskUser();
-            Rooms room = new Rooms(roomName, roomSize, seats, hasProjector, hasWhiteBoard);
-            rooms.Add(room);
+
+            if (roomSize == "Large")
+            {
+                Console.WriteLine("Adding a Large Room...");
+                int seatLimit = 120;
+                rooms.Add(new LargeRoom(roomName, roomSize, seats, seatLimit, hasProjector, hasWhiteBoard));
+            }
+            else if (roomSize == "Medium")
+            {
+                Console.WriteLine("Adding a Medium Room...");
+                int seatLimit = 60;
+                rooms.Add(new MediumRoom(roomName, roomSize, seats, seatLimit, hasProjector, hasWhiteBoard));
+            }
+            else if (roomSize == "Small")
+            {
+                Console.WriteLine("Adding a Small Room...");
+                int seatLimit = 15;
+                rooms.Add(new SmallRoom(roomName, roomSize, seats, seatLimit));
+            }
+
+            ShowList(rooms);
+            //IRoom room = new IRoom(roomName, roomS)
+
+            //IRoom room = new IRoom(roomName, roomType, )
+            //Rooms room = new Rooms(roomName, roomSize, seats, hasProjector, hasWhiteBoard);
+            //rooms.Add(room);
         }
+        //public static void CreateARoom(List<Rooms> rooms)
+        //{
+        //    Console.WriteLine("Enter name of the room: ");
+        //    string? roomName = Console.ReadLine();
+        //    string roomSize = RoomSize();
+        //    Console.WriteLine("Enter how many seats: ");
+        //    int seats = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Do you need a projector? Y/N");
+        //    bool hasProjector = AskUser();
+        //    Console.WriteLine("Do you need a whiteboard? Y/N");
+        //    bool hasWhiteBoard = AskUser();
+        //    Rooms room = new Rooms(roomName, roomSize, seats, hasProjector, hasWhiteBoard);
+        //    rooms.Add(room);
+        //}
+        //public static string RoomSize()
+        //{
+
+        //    Console.WriteLine("1.Large\n2.Medium\n3.Small");
+        //    string? option;
+        //    string? size;
+        //    switch (option = Console.ReadLine())
+        //    {
+        //        case "1":
+        //            size = "Large";
+
+        //            break;
+        //        case "2":
+        //            size = "Medium";
+        //            break;
+        //        case "3":
+        //            size = "Small";
+        //            break;
+        //        default:
+        //            Console.WriteLine("Invalid choice");
+        //            return RoomSize();
+        //    }
+        //    return size;
+        //}
+
         public static string RoomSize()
         {
-
-            Console.WriteLine("1.Group room\n2.Medium\n3.Large");
+            Console.WriteLine("Choose room size:\n1.Large\n2.Medium\n3.Small");
             string? option;
-            string? size;
+            string size = ""; 
+
             switch (option = Console.ReadLine())
             {
                 case "1":
-                    size = "Group room";
+                    size = "Large";
                     break;
                 case "2":
                     size = "Medium";
                     break;
                 case "3":
-                    size = "Large";
+                    size = "Small";
                     break;
                 default:
-                    Console.WriteLine("Invalid choice");
-                    return RoomSize();
+                    Console.WriteLine("Invalid choice, please choose again.");
+                    return RoomSize(); 
             }
             return size;
         }
+
         public static bool AskUser()
         {
             while (true)
@@ -63,6 +128,37 @@ namespace BokningssystemGrupp6.Classes.LokalClasses
                     Console.WriteLine("invalid option");
                 }
 
+            }
+        }
+
+
+        public static void ShowList(List<IRoom> rooms)
+        {
+            foreach (var room in rooms)
+            {
+                Console.WriteLine($"Room Type: {room.GetType().Name}");
+                Console.WriteLine($"Room Name: {room.RoomName}");
+                Console.WriteLine($"Room Type Description: {room.RoomType}");
+                Console.WriteLine($"Seat Amount: {room.SeatAmount}");
+
+                if (room is LargeRoom largeRoom)
+                {
+                    Console.WriteLine($"Seat Limit: {largeRoom.SeatLimit}");
+                    Console.WriteLine($"Has Projector: {largeRoom.HasProjector}");
+                    Console.WriteLine($"Has Whiteboard: {largeRoom.HasWhiteboard}");
+                }
+                else if (room is MediumRoom mediumRoom)
+                {
+                    Console.WriteLine($"Seat Limit: {mediumRoom.SeatLimit}");
+                    Console.WriteLine($"Has Projector: {mediumRoom.HasProjector}");
+                    Console.WriteLine($"Has Whiteboard: {mediumRoom.HasWhiteboard}");
+                }
+                else if (room is SmallRoom smallRoom)
+                {
+                    Console.WriteLine($"Seat Limit: {smallRoom.SeatLimit}");
+                }
+
+                Console.WriteLine("----------------------");
             }
         }
 
--- BokningssystemGrupp6/Program.cs
+++ BokningssystemGrupp6/Program.cs
@@ -1,36 +1,77 @@
 ﻿using System.Drawing;
+using BokningssystemGrupp6.Classes;
 using BokningssystemGrupp6.Classes.LokalClasses;
+using BokningssystemGrupp6.Interfaces;
 
 namespace BokningssystemGrupp6
 {
     internal class Program
     {
         //Lista för lokaler (string roomName, string size, int maxPeople, bool hasWhiteboard, bool hasProjector)
-        public static List<Rooms> RoomsInfo = new List<Rooms>();
+        //public static List<Rooms> RoomsInfo = new List<Rooms>();
+        //public static List<IRoom> rooms2 = new List<IRoom>();
         //Lista för bokningar (string userName, string roomName, double startTime, double endTime)
-        public static List<Bookings> BookingsInfo = new List<Bookings>();
+        //public static List<Bookings> BookingsInfo = new List<Bookings>();
         static void Main(string[] args)
         {
-            //Sample data
-            Rooms Delfinen = new("Delfinen","GroupRoom", 8, false, false);
-            Rooms Hajen = new("Hajen","GroupRoom", 8, false, false);
-            Rooms Valen = new("Valen","Medium",20, false, false);
-            Rooms Maneten = new("Maneten","Medium", 20, true, false);
-            Rooms Krabban = new("Krabban","Large", 40, true, true);
-            Rooms Aborren = new("Aborren","Large", 40, true, true);
-            Rooms Laxen = new("Laxen","Large", 40, true, true);
-            Rooms Korallen = new("Korallen", "Large", 40, true, true);
 
-            RoomsInfo.Add(Delfinen);
-            RoomsInfo.Add(Hajen);
-            RoomsInfo.Add(Valen);
-            RoomsInfo.Add(Maneten);
-            RoomsInfo.Add(Krabban);
-            RoomsInfo.Add(Aborren);
-            RoomsInfo.Add(Laxen);
-            RoomsInfo.Add(Korallen);
+
+            List<IRoom> rooms = new List<IRoom>();
+            rooms.Add(new LargeRoom("Katt", "Large", 100, 120, true, true));
+            rooms.Add(new MediumRoom("Hund Room B", "Medium", 50, 60, true, false));
+            rooms.Add(new SmallRoom("Kanin", "Small", 10, 15));
+
+            foreach (var room in rooms)
+            {
+                Console.WriteLine($"Room Type: {room.GetType().Name}");
+                Console.WriteLine($"Room Name: {room.RoomName}");
+                Console.WriteLine($"Room Type Description: {room.RoomType}");
+                Console.WriteLine($"Seat Amount: {room.SeatAmount}");
+
+                if (room is LargeRoom largeRoom)
+                {
+                    Console.WriteLine($"Seat Limit: {largeRoom.SeatLimit}");
+                    Console.WriteLine($"Has Projector: {largeRoom.HasProjector}");
+                    Console.WriteLine($"Has Whiteboard: {largeRoom.HasWhiteboard}");
+                }
+                else if (room is MediumRoom mediumRoom)
+                {
+                    Console.WriteLine($"Seat Limit: {mediumRoom.SeatLimit}");
+                    Console.WriteLine($"Has Projector: {mediumRoom.HasProjector}");
+                    Console.WriteLine($"Has Whiteboard: {mediumRoom.HasWhiteboard}");
+                }
+                else if (room is SmallRoom smallRoom)
+                {
+                    Console.WriteLine($"Seat Limit: {smallRoom.SeatLimit}");
+                }
+
+                Console.WriteLine("----------------------");
+            }
+
+
+
+            //Sample data
+            //Rooms Delfinen = new("Delfinen","GroupRoom", 8, false, false);
+            //Rooms Hajen = new("Hajen","GroupRoom", 8, false, false);
+            //Rooms Valen = new("Valen","Medium",20, false, false);
+            //Rooms Maneten = new("Maneten","Medium", 20, true, false);
+            //Rooms Krabban = new("Krabban","Large", 40, true, true);
+            //Rooms Aborren = new("Aborren","Large", 40, true, true);
+            //Rooms Laxen = new("Laxen","Large", 40, true, true);
+            //Rooms Korallen = new("Korallen", "Large", 40, true, true);
+
+            //RoomsInfo.Add(Delfinen);
+            //RoomsInfo.Add(Hajen);
+            //RoomsInfo.Add(Valen);
+            //RoomsInfo.Add(Maneten);
+            //RoomsInfo.Add(Krabban);
+            //RoomsInfo.Add(Aborren);
+            //RoomsInfo.Add(Laxen);
+            //RoomsInfo.Add(Korallen);
 
             Console.WriteLine("Hello, World!");
+
+            Menu.MainMenu( rooms );
         }
     }
 }
